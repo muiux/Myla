cc0e7a6b1939ac015f072dfb0ddc59be604d9213
hoaduong7 <hoaduong@protonmail.com>
2023-05-30 12:36:46 +0100

    css

--- myla-app/pages/reset-password.js
+++ myla-app/pages/reset-password.js
@@ -9,9 +9,6 @@ export default function ResetPassword(){
         <title>Reset Password</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" /> 
         <meta content="Webflow" name="generator" />
-        <link href="css/normalize.css" rel="stylesheet" type="text/css" />
-        <link href="css/webflow.css" rel="stylesheet" type="text/css" />
-        <link href="css/myla-d39d21.webflow.css" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com" rel="preconnect" />
         <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous" />
         <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
