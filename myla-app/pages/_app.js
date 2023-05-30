1de33e74c67841fd2e8ad35f19339b50c1ba15b8
hoaduong7 <hoaduong@protonmail.com>
2023-05-30 12:33:53 +0100

    Update _app.js

--- myla-app/pages/_app.js
+++ myla-app/pages/_app.js
@@ -9,13 +9,13 @@ export default function App({ Component, pageProps }) {
       <Script 
         src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" 
         onLoad={() =>
-          console.log(`3script loaded correctly, window.FB has been populated`)
+          console.log(`webfont script loaded correctly`)
         }
       />
     <Script 
         src="/js/extra.js" 
         onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
+          console.log(`extra script loaded correctly`)
         }
     />
 
@@ -23,13 +23,13 @@ export default function App({ Component, pageProps }) {
     <Script 
         src='https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=63c8475ba85eb1570c45b959' 
         onLoad={() =>
-          console.log(`2script loaded correctly, window.FB has been populated`)
+          console.log(`jquery loaded correctly`)
         }
     />
     <Script 
         src="/js/webflow.js" 
         onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
+          console.log(`webflow script loaded correctly`)
         }
     />
     </>
