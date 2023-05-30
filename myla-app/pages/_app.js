ae13bc24c60aa9abbb321ffcc3d639b0f6cf536f
hoaduong7 <hoaduong@protonmail.com>
2023-05-30 12:04:41 +0100

    first batch

---
+++ myla-app/pages/_app.js
@@ -0,0 +1,37 @@
+import '@/styles/normalize.css'
+import '@/styles/webflow.css'
+import '@/styles/myla-d39d21.webflow.css'
+import Script from 'next/script'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Script 
+        src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" 
+        onLoad={() =>
+          console.log(`3script loaded correctly, window.FB has been populated`)
+        }
+      />
+    <Script 
+        src="/js/extra.js" 
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+    />
+
+    <Component {...pageProps} />
+    <Script 
+        src='https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=63c8475ba85eb1570c45b959' 
+        onLoad={() =>
+          console.log(`2script loaded correctly, window.FB has been populated`)
+        }
+    />
+    <Script 
+        src="/js/webflow.js" 
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+    />
+    </>
+  )
+}
