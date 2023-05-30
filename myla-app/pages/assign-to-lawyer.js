536a8b634f51b6bcafa5755a25589d6d0e559653
hoaduong7 <hoaduong@protonmail.com>
2023-05-30 12:30:48 +0100

    second batch

---
+++ myla-app/pages/assign-to-lawyer.js
@@ -0,0 +1,348 @@
+import React from "react";
+import Head from 'next/head'
+
+export default function AssignToLawyer(){
+    return(
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <title>Assign to Lawyer</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" /> 
+        <meta content="Webflow" name="generator" />
+        <link href="css/normalize.css" rel="stylesheet" type="text/css" />
+        <link href="css/webflow.css" rel="stylesheet" type="text/css" />
+        <link href="css/myla-d39d21.webflow.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com" rel="preconnect" />
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous" />
+        <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+        <link href="/images/webclip.png" rel="apple-touch-icon" />
+      </Head>
+      <>
+  <div
+    data-animation="default"
+    data-collapse="medium"
+    data-duration={400}
+    data-easing="ease"
+    data-easing2="ease"
+    role="banner"
+    className="navbar-3 w-nav"
+  >
+    <div className="container-3 w-container">
+      <a
+        href="my-intakes-dashboard"
+        className="link-block-7 w-inline-block"
+      >
+        <img
+          src="https://uploads-ssl.webflow.com/63abf4b38af6f93334f534b0/63abf671c73ddf94d00ea298_Image%2027-12-2022%20at%203.23%20pm.jpg"
+          loading="lazy"
+          alt=""
+          className="image-4"
+          width={160}
+        />
+      </a>
+      <h2 className="heading-3">my leads area</h2>
+      <nav role="navigation" className="nav-menu w-nav-menu">
+        <a href="#" className="nav-link-4 w-nav-link">
+          Help
+        </a>
+        <a href="#" className="nav-link-5 w-nav-link">
+          Settings
+        </a>
+        <img
+          src="https://uploads-ssl.webflow.com/63abf4b38af6f93334f534b0/63abf671c73ddf083f0ea295_account.png"
+          loading="lazy"
+          data-w-id="356f5317-d528-0c9e-cfe5-00e5fb0ceb2e"
+          alt=""
+          className="image-5"
+          width={50}
+        />
+      </nav>
+      <div className="w-nav-button">
+        <div className="w-icon-nav-menu" />
+      </div>
+    </div>
+  </div>
+  <div className="w-container">
+    <div className="form-block w-form">
+      <form
+        id="email-form"
+        name="email-form"
+        data-name="Email Form"
+        method="get"
+      >
+        <label htmlFor="First-Name" className="field-label">
+          Primary Lawyer*
+        </label>
+        <div data-hover="false" data-delay={0} className="w-dropdown">
+          <div className="w-dropdown-toggle">
+            <div className="w-icon-dropdown-toggle" />
+            <div>Dropdown</div>
+          </div>
+          <nav className="w-dropdown-list">
+            <a href="#" className="w-dropdown-link">
+              Link 1
+            </a>
+            <a href="#" className="w-dropdown-link">
+              Link 2
+            </a>
+            <a href="#" className="w-dropdown-link">
+              Link 3
+            </a>
+          </nav>
+        </div>
+        <label htmlFor="First-Name-2" className="field-label">
+          Secondary Lawyer/Legal Assistant
+        </label>
+        <div data-hover="false" data-delay={0} className="w-dropdown">
+          <div className="w-dropdown-toggle">
+            <div className="w-icon-dropdown-toggle" />
+            <div>Dropdown</div>
+          </div>
+          <nav className="w-dropdown-list">
+            <a href="#" className="w-dropdown-link">
+              Link 1
+            </a>
+            <a href="#" className="w-dropdown-link">
+              Link 2
+            </a>
+            <a href="#" className="w-dropdown-link">
+              Link 3
+            </a>
+          </nav>
+        </div>
+        <a href="#" className="button-8 w-button">
+          + Add Lawyer to File
+        </a>
+        <label htmlFor="Client-ref-no-2" className="field-label">
+          Add Client Reference No. (optional)
+        </label>
+        <input
+          type="text"
+          className="w-input"
+          maxLength={256}
+          name="Client-ref-no-2"
+          data-name="Client Ref No 2"
+          placeholder="Client ref no."
+          id="Client-ref-no-2"
+          required=""
+        />
+        <div className="div-block-2" />
+        <label htmlFor="First-Name-2" className="field-label">
+          <strong>Review Client Information:</strong>
+        </label>
+        <label htmlFor="First-Name-2" className="field-label">
+          Name of Client 1
+        </label>
+        <input
+          type="text"
+          className="w-input"
+          maxLength={256}
+          name="First-Name"
+          data-name="First Name"
+          placeholder="First name"
+          id="First-Name"
+          required=""
+        />
+        <input
+          type="text"
+          className="w-input"
+          maxLength={256}
+          name="Last-Name"
+          data-name="Last Name"
+          placeholder="Last name"
+          id="Last-Name"
+          required=""
+        />
+        <label htmlFor="email" className="field-label">
+          Client Email
+        </label>
+        <input
+          type="email"
+          className="w-input"
+          maxLength={256}
+          name="email"
+          data-name="Email"
+          placeholder="Email"
+          id="email"
+          required=""
+        />
+        <label htmlFor="Phone" className="field-label">
+          Client Phone{" "}
+        </label>
+        <input
+          type="email"
+          className="w-input"
+          maxLength={256}
+          name="Phone"
+          data-name="Phone"
+          placeholder="Phone"
+          id="Phone"
+          required=""
+        />
+        <a href="#" className="button-8 w-button">
+          + Add Client to File
+        </a>
+        <label htmlFor="Client-ref-no" className="field-label">
+          Client Reference No. (optional)
+        </label>
+        <input
+          type="text"
+          className="w-input"
+          maxLength={256}
+          name="Client-ref-no"
+          data-name="Client ref no"
+          placeholder="Client ref no."
+          id="Client-ref-no"
+          required=""
+        />
+        <div className="div-block-2" />
+        <label htmlFor="email-2" className="field-label">
+          Area of Law
+        </label>
+        <a
+          data-w-id="b43546b0-7987-22fc-984b-3c2e8a43fce8"
+          style={{
+            backgroundColor: "rgba(16,75,145,0.23)",
+            color: "rgba(51,51,51,0.78)"
+          }}
+          href="#"
+          className="button-6 w-button"
+        >
+          Family Law
+        </a>
+        <a
+          data-w-id="b43546b0-7987-22fc-984b-3c2e8a43fcea"
+          style={{
+            backgroundColor: "rgba(16,75,145,0.23)",
+            color: "rgba(51,51,51,0.78)"
+          }}
+          href="#"
+          className="button-5 w-button"
+        >
+          Commercial Law
+        </a>
+        <a
+          data-w-id="b43546b0-7987-22fc-984b-3c2e8a43fcec"
+          style={{
+            backgroundColor: "rgba(16,75,145,0.23)",
+            color: "rgba(51,51,51,0.78)"
+          }}
+          href="#"
+          className="button-4 w-button"
+        >
+          Wills and Estates
+        </a>
+        <a
+          data-w-id="b43546b0-7987-22fc-984b-3c2e8a43fcee"
+          style={{
+            backgroundColor: "rgba(16,75,145,0.23)",
+            color: "rgba(51,51,51,0.78)"
+          }}
+          href="#"
+          className="button-7 w-button"
+        >
+          Real Estate
+        </a>
+        <div className="div-block-2" />
+        <label htmlFor="email-2" className="field-label">
+          Intake Form
+        </label>
+        <div data-delay={0} data-hover="false" className="w-dropdown">
+          <div className="dropdown-toggle-6 w-dropdown-toggle">
+            <div className="w-icon-dropdown-toggle" />
+            <div className="text-block-23">Select intake form</div>
+          </div>
+          <nav className="w-dropdown-list">
+            <a href="#" className="w-dropdown-link">
+              Link 1
+            </a>
+            <a href="#" className="w-dropdown-link">
+              Link 2
+            </a>
+            <a href="#" className="w-dropdown-link">
+              Link 3
+            </a>
+          </nav>
+        </div>
+        <div className="div-block-2" />
+        <label htmlFor="Deadline" className="field-label">
+          Deadline
+        </label>
+        <input
+          type="email"
+          className="w-input"
+          maxLength={256}
+          name="Deadline"
+          data-name="Deadline"
+          placeholder="dd/mm/yyyy or estimate"
+          id="Deadline"
+          required=""
+        />
+        <div className="div-block-2" />
+        <label htmlFor="field" className="field-label">
+          Notes for lawyer
+        </label>
+        <textarea
+          id="field"
+          name="field"
+          maxLength={5000}
+          data-name="field"
+          placeholder=""
+          className="w-input"
+          defaultValue={""}
+        />
+        <div className="div-block-2" />
+        <label htmlFor="field-2" className="field-label">
+          <strong>Review First Draft and Notes</strong>
+        </label>
+        <div className="text-block-29">
+          This is where we embed the word doc.
+        </div>
+        <label htmlFor="field-2" className="field-label">
+          Additional Notes for Lawyer
+        </label>
+        <textarea
+          id="field-2"
+          name="field-2"
+          maxLength={5000}
+          data-name="Field 2"
+          placeholder=""
+          className="w-input"
+          defaultValue={""}
+        />
+        <a href="#" className="submit-button w-button">
+          Create Client File and Assign to Lawyer
+        </a>
+      </form>
+      <div className="w-form-done">
+        <div>Thank you! Your submission has been received!</div>
+      </div>
+      <div className="w-form-fail">
+        <div>Oops! Something went wrong while submitting the form.</div>
+      </div>
+    </div>
+  </div>
+  <div className="div-block-11">
+    <div className="section-13 wf-section">
+      <div className="text-block-16">Account Name</div>
+    </div>
+    <div className="section-14 wf-section">
+      <a href="#" className="link-11">
+        Profile
+      </a>
+      <a href="#" className="link-8">
+        Switch Account
+      </a>
+      <a href="#" className="link-8">
+        Log Out
+      </a>
+      <a href="#" className="link-8">
+        Manage Linked Accounts
+      </a>
+    </div>
+  </div>
+</>
+
+    </>
+    )
+}
---
+++ myla-app/pages/update-password.js
@@ -0,0 +1,120 @@
+import React from "react";
+import Head from 'next/head'
+
+export default function UpdatePassword(){
+    return(
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <title>Update Password</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" /> 
+        <meta content="Webflow" name="generator" />
+        <link href="css/normalize.css" rel="stylesheet" type="text/css" />
+        <link href="css/webflow.css" rel="stylesheet" type="text/css" />
+        <link href="css/myla-d39d21.webflow.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com" rel="preconnect" />
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous" />
+        <link href="/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+        <link href="/images/webclip.png" rel="apple-touch-icon" />
+      </Head>
+      <>
+  <div className="wf-section">
+    <div className="container-6 w-container">
+      <a href="#" className="link-block-2 w-inline-block">
+        <img
+          src="https://uploads-ssl.webflow.com/63abf4b38af6f93334f534b0/63acc6825b640981f1ff35ed_Image%2028-12-2022%20at%202.42%20pm.jpg"
+          loading="lazy"
+          alt=""
+          className="image-6"
+          width={177}
+        />
+      </a>
+    </div>
+  </div>
+  <div className="w-users-userformpagewrap page-wrapper-4">
+    <div className="w-users-userupdatepasswordformwrapper update-password-block">
+      <div
+        tabIndex={-1}
+        className="w-users-userformsuccessstate w-form-success"
+      >
+        <div className="w-users-userformheader">
+          <img
+            src="https://d3e54v103j8qbb.cloudfront.net/img/thumbs-up-72.cbcaec93bc.svg"
+            alt=""
+          />
+          <h2>Password updated</h2>
+        </div>
+        <p>
+          Your password was updated successfully. You were automatically logged
+          in and can continue your session.
+        </p>
+        <a href="index" className="w-button">
+          Go to Homepage
+        </a>
+      </div>
+      <form method="post" data-wf-user-form-type="updatePassword">
+        <div className="w-users-userformheader">
+          <h2 className="heading-7">Update Password</h2>
+        </div>
+        <p id="wf-update-password-paragraph" className="paragraph">
+          Please enter your new password in the field below. Make sure you are
+          not using your current password.
+        </p>
+        <label htmlFor="wf-update-password" className="field-label-6">
+          New Password
+        </label>
+        <input
+          type="password"
+          maxLength={256}
+          placeholder=""
+          name="Password"
+          id="wf-update-password"
+          aria-describedby="wf-update-password-paragraph"
+          className="w-input"
+          required=""
+          data-wf-user-form-input-type="password"
+        />
+        <input
+          type="submit"
+          defaultValue="Update Password"
+          data-wait="Please wait..."
+          className="w-users-userformbutton submit-button-4 w-button"
+        />
+      </form>
+      <div
+        style={{ display: "none" }}
+        data-wf-user-form-error="true"
+        className="w-users-userformerrorstate w-form-fail"
+      >
+        <div
+          className="user-form-error-msg"
+          wf-update-pw-form-general-error-error="There was an error updating your password. Please try again, or contact us if you continue to have problems."
+          wf-update-pw-form-weak-password-error="Your password must be at least 8 characters."
+        >
+          There was an error updating your password. Please try again, or
+          contact us if you continue to have problems.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="section-5 wf-section">
+    <img
+      src="https://uploads-ssl.webflow.com/63abf4b38af6f93334f534b0/63acc7e9d827c572b451f523_Image%2028-12-2022%20at%202.48%20pm.jpg"
+      loading="lazy"
+      alt=""
+      className="image-24"
+      width={226}
+    />
+    <img
+      src="https://uploads-ssl.webflow.com/63abf4b38af6f93334f534b0/63ad0a71461adb0681d55f14_Image%2028-12-2022%20at%207.32%20pm.jpg"
+      loading="lazy"
+      alt=""
+      className="image-23"
+      width={464}
+    />
+  </div>
+</>
+
+    </>
+    )
+}
